a287cfd78c5db9e8e5b8d3736b712adc64f57e82
rahsharma6969 <rahsharma2004@gmail.com>
2025-06-20 12:53:43 +0530

    Add Fibonacci series implementation using dynamic programming

---
+++ DP/FibonacciSeries.py
@@ -0,0 +1,14 @@
+class Solution:
+    def __init__(self):
+        self.dp = {}
+
+    def fib(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+        
+        if n in self.dp:
+            return self.dp[n]
+        
+        self.dp[n] =  self.fib(n-1) + self.fib( n - 2)
+
+        return self.dp[n]
